a919650a7a4ea5c3543a6f8fc71aea4c54dbb221
Timur Gafarov <gecko0307@gmail.com>
2025-11-18 21:40:14 +0300

    Fix

--- src/vehicle.d
+++ src/vehicle.d
@@ -292,10 +292,7 @@ class Vehicle: EntityComponent
         float rpmWheel = carSpeed * 1000.0f / (60.0f * 2.0f * PI * effectiveRadius);
         float rpmClutch = rpmWheel * transmissionRatio;
         float rpmFree = lerp(rpmIdle, rpmMax, throttle);
-        rpm = lerp(
-            max2(rpmIdle, rpmClutch),
-            max3(rpmIdle, (rpmFree - rpmClutch) / engineInertia, rpmClutch),
-            effectiveClutch);
+        rpm = lerp(rpmIdle, max3(rpmIdle, (rpmFree - rpmClutch) / engineInertia, rpmClutch), effectiveClutch);
         float engineTorque = engineTorqueCurve(rpm) * throttle;
         float axleTorque = sign(gearRatio) * engineTorque * effectiveClutch * transmissionRatio;
         
